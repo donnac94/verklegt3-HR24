09a26c32fcfc874a22ba92c2a6318ac746098108
HelgaS-24 <helgasigurjonsd@gmail.com>
2024-12-02 12:08:49 +0000

    logic_layer functions

---
+++ Logic_Layer/WorkOrderLogic.py
@@ -0,0 +1,18 @@
+def CreateWorkOrder():
+    pass
+
+
+def ListWorkOrders():
+    pass
+
+
+def ChangeWorkOrderInfo():
+    pass
+
+
+def CloseWorkOrder():
+    pass
+
+
+def ReopenWorkOrder():
+    pass
