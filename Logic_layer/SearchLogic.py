83a78723d207e364e289cc1c945b1ddc231dc78e
Gudniemil24 <gudni24@ru.is>
2024-12-11 12:04:48 +0000

    search

--- Logic_layer/SearchLogic.py
+++ Logic_layer/SearchLogic.py
@@ -17,14 +17,16 @@ class SearchLogic:
     
     
     def search_by_location(self, location_id):
-        while location_id != None or location_id < 0 or location_id > :
+        while location_id != None or location_id < 0 or location_id > 100:
+            if EmployeeLogic
+                
             
-    
     def search_employee_by_ssn(self, ssn):
+        while ssn != None or ssn < 0 or ssn > 10:
         
     
     def search_property_id(self, property_id):
-        pass
+        while property_id != None or property_id < 0 or property_id > 100:
     
     def search_workorder_id(self, workorder_id):
-        pass
+        while workorder_id != None or workorder_id < 0 or workorder_id > 100:
